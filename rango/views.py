be1fa2fd25adfbd4f83a0f9957701de309fd0b13
Ronnie Mathew <rkemathew@yahoo.com>
2014-12-02 13:49:19 -0500

    Add

--- rango/views.py
+++ rango/views.py
@@ -1,10 +1,13 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Rango Says: Hello World!<br/>Here is the <a href='./about'>about</a> page.")
+    context = RequestContext(request)
+    
+    context_dict = {'boldmessage': "I am bold font from the context"}
+    
+    return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
-    return HttpResponse("Rango Says: Here is the about page.<br/>Click <a href='/rango'>here</a> to go back.")
+    return render_to_response('rango/about.html')
+    
